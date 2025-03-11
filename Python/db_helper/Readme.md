4d0ae8cf7ed5613b4b7d831ee17459cb1ee48a29
PavlasProtopas <ghostezlarin@gmail.com>
2025-03-12 00:02:17 +0300

    db_helper module for copy csv file in databse.

---
+++ Python/db_helper/Readme.md
@@ -0,0 +1,5 @@
+# db_helper
+## db_copy.py
+Util for copy data.csb file in database.
+## db_const.py
+Const for connection to database.
---
+++ Python/db_helper/db_config.py
@@ -0,0 +1,6 @@
+user = "*"
+password = "*"
+host = "*"
+port = "*"
+database = "*"
+SCHEMA_NAME = "test"  # Имя схемы, в которой будет создана таблица
